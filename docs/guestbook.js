0afdee1b41b49daad475bda0c63e7cf4e8113888
Tosin Amuda <tosinamuda@gmail.com>
2020-10-14 14:58:29 +0100

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://3b1d9fab.eu-gb.apigw.appdomain.cloud/guestbook-api';
+const apiUrl = 'https://3b1d9fab.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
